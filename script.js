69d51b2c2e73b34bf75c3955a1b0709f8003382c
J.Hergert <julian.konken@gmx.net>
2026-02-08 23:44:56 +0100

    add cardNum N styling

--- script.js
+++ script.js
@@ -23,11 +23,16 @@ function getCardTemplate(index) {
 
   return ` 
     <section class="card bg_${preLoadCase[index].types[0].type.name}">
-      <h1>${pokemoName}</h1>
-      <img src="${preLoadCase[index].sprites.other.dream_world.front_default}" alt="">
-      <div class="types" id="types_${index}">
-        <p class="type_one">${preLoadCase[index].types[0].type.name}</p>
-      </div>
+      <header>
+        <h1>${pokemoName}</h1>
+        <div class="cardNumber">#${preLoadCase[index].id}</div>
+      </header>
+      <figure>
+        <img src="${preLoadCase[index].sprites.other.dream_world.front_default}" alt="">
+        <figcaption class="types" id="types_${index}">
+          <p class="type_one">${preLoadCase[index].types[0].type.name}</p>
+        </figcaption>
+      </figure>
     </section>
   `
 }
